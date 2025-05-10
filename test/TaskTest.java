3cf4060510334d19dccccfdb386727af24d45547
hsinyentsai <hytsai@seas.upenn.edu>
2025-05-10 15:22:24 -0400

    update test cases for task class

--- test/TaskTest.java
+++ test/TaskTest.java
@@ -5,11 +5,14 @@ import static org.junit.Assert.*;
 public class TaskTest {
 
     @Test
-    public void taskTest() {
-    }
-    @Test
     public void getDescriptionTest() {
         Task task = new Task("Test the bot");
         assertEquals("Test the bot", task.getDescription());
+    }
+
+    @Test
+    public void getDescriptionTest2() {
+        Task task = new Task("");
+        assertEquals("", task.getDescription());
     }
 }
